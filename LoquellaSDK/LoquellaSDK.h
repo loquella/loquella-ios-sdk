e68b3ea4d4a37090f6f4c17e136422e5b450140d
Rasmus Styrk <styrken@gmail.com>
2018-02-19 14:57:33 +0100

    Initial Commit

---
+++ LoquellaSDK/LoquellaSDK.h
@@ -0,0 +1,19 @@
+//
+//  LoquellaSDK.h
+//  LoquellaSDK
+//
+//  Created by Rasmus Styrk on 19/02/2018.
+//  Copyright © 2018 House of Code ApS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoquellaSDK.
+FOUNDATION_EXPORT double LoquellaSDKVersionNumber;
+
+//! Project version string for LoquellaSDK.
+FOUNDATION_EXPORT const unsigned char LoquellaSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoquellaSDK/PublicHeader.h>
+
+

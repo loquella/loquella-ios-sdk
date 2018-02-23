879d14a6e5b77931740b8aa0998d8b336a0de2f7
Rasmus Styrk <styrken@gmail.com>
2018-02-23 08:23:28 +0100

    Updated sample

--- LoquellaSample/ViewController.swift
+++ LoquellaSample/ViewController.swift
@@ -12,16 +12,16 @@ import LoquellaSDK
 class ViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var versionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.titleLabel.text = "Unknown translation".translate()
+        self.titleLabel.translate() // Autotranslate from nib
         
-        print("We should translate this text, right?".translate())
-        print("My name is :1".translate("Hello World"))
-        
+        self.descriptionLabel.text = "We hope you enjoy our project".translate()
+        self.versionLabel.text = "Version: :1".translate("1.0")
     }
 }
-
